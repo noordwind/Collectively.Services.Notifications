d6bd879081d88c01455920e52f99905e6522316c
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2017-04-04 11:35:14 +0200

    notification service initialized

---
+++ Collectively.Services.Notifications/Framework/Bootstrapper.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using Autofac;
+using Collectively.Common.Exceptionless;
+using Collectively.Common.Extensions;
+using Collectively.Common.Nancy;
+using Collectively.Common.RabbitMq;
+using Collectively.Common.Security;
+using Collectively.Common.Services;
+using Collectively.Messages.Commands;
+using Collectively.Messages.Events;
+using Microsoft.Extensions.Configuration;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Bootstrappers.Autofac;
+using Nancy.Configuration;
+using Newtonsoft.Json;
+using NLog;
+using RawRabbit.Configuration;
+
+namespace Collectively.Services.Notifications.Framework
+{
+    public class Bootstrapper : AutofacNancyBootstrapper
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private static IExceptionHandler _exceptionHandler;
+        private readonly IConfiguration _configuration;
+        private static readonly string DecimalSeparator = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+        private static readonly string InvalidDecimalSeparator = DecimalSeparator == "." ? "," : ".";
+        public static ILifetimeScope LifetimeScope { get; private set; }
+
+        public Bootstrapper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+#if DEBUG
+        public override void Configure(INancyEnvironment environment)
+        {
+            base.Configure(environment);
+            environment.Tracing(enabled: false, displayErrorTraces: true);
+        }
+#endif
+
+        protected override void ConfigureApplicationContainer(ILifetimeScope container)
+        {
+            base.ConfigureApplicationContainer(container);
+
+            container.Update(builder =>
+            {
+                builder.RegisterType<CustomJsonSerializer>().As<JsonSerializer>().SingleInstance();
+                builder.RegisterInstance(_configuration.GetSettings<ExceptionlessSettings>()).SingleInstance();
+                builder.RegisterType<ExceptionlessExceptionHandler>().As<IExceptionHandler>().SingleInstance();
+                builder.RegisterType<Handler>().As<IHandler>();
+
+                var assembly = typeof(Startup).GetTypeInfo().Assembly;
+                builder.RegisterAssemblyTypes(assembly).AsClosedTypesOf(typeof(IEventHandler<>));
+                builder.RegisterAssemblyTypes(assembly).AsClosedTypesOf(typeof(ICommandHandler<>));
+                SecurityContainer.Register(builder, _configuration);
+                RabbitMqContainer.Register(builder, _configuration.GetSettings<RawRabbitConfiguration>());
+            });
+            LifetimeScope = container;
+        }
+
+        protected override void RequestStartup(ILifetimeScope container, IPipelines pipelines, NancyContext context)
+        {
+            pipelines.OnError.AddItemToEndOfPipeline((ctx, ex) =>
+            {
+                _exceptionHandler.Handle(ex, ctx.ToExceptionData(),
+                    "Request details", "Collectively", "Service", "Remarks");
+
+                return ctx.Response;
+            });
+        }
+
+        protected override void ApplicationStartup(ILifetimeScope container, IPipelines pipelines)
+        {
+            pipelines.BeforeRequest += (ctx) =>
+            {
+                FixNumberFormat(ctx);
+
+                return null;
+            };
+            pipelines.AfterRequest += (ctx) =>
+            {
+                ctx.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+                ctx.Response.Headers.Add("Access-Control-Allow-Methods", "POST,PUT,GET,OPTIONS,DELETE");
+                ctx.Response.Headers.Add("Access-Control-Allow-Headers",
+                    "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+            };
+            pipelines.SetupTokenAuthentication(container);
+            _exceptionHandler = container.Resolve<IExceptionHandler>();
+            Logger.Info("Collectively.Services.Notifications API has started.");
+        }
+
+        private void FixNumberFormat(NancyContext ctx)
+        {
+            if (ctx.Request.Query == null)
+                return;
+
+            var fixedNumbers = new Dictionary<string, double>();
+            foreach (var key in ctx.Request.Query)
+            {
+                var value = ctx.Request.Query[key].ToString();
+                if (!value.Contains(InvalidDecimalSeparator))
+                    continue;
+
+                var number = 0;
+                if (int.TryParse(value.Split(InvalidDecimalSeparator[0])[0], out number))
+                    fixedNumbers[key] = double.Parse(value.Replace(InvalidDecimalSeparator, DecimalSeparator));
+            }
+            foreach (var fixedNumber in fixedNumbers)
+            {
+                ctx.Request.Query[fixedNumber.Key] = fixedNumber.Value;
+            }
+        }
+    }
+}
---
+++ Collectively.Services.Notifications/Program.cs
@@ -0,0 +1,18 @@
+﻿using Collectively.Common.Host;
+using Collectively.Services.Notifications.Framework;
+
+namespace Collectively.Services.Notifications
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            WebServiceHost
+                .Create<Startup>(port: 10007)
+                .UseAutofac(Bootstrapper.LifetimeScope)
+                .UseRabbitMq(queueName: typeof(Program).Namespace)
+                .Build()
+                .Run();
+        }
+    }
+}
