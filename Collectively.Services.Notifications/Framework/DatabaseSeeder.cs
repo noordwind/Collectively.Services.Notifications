2239e5f34003a94d5db11fc89e2aae89621882d5
Grzegorz Paśnik <grzegorz.pasnik@noordwind.com>
2017-05-15 14:31:00 +0200

    german language support

--- Collectively.Services.Notifications/Framework/DatabaseSeeder.cs
+++ Collectively.Services.Notifications/Framework/DatabaseSeeder.cs
@@ -23,22 +23,31 @@ namespace Collectively.Services.Notifications.Framework
                 {
                     new LocalizedResource("category:praise", "en-gb", "praise"),
                     new LocalizedResource("category:praise", "pl-pl", "pochwała"),
+                    new LocalizedResource("category:praise", "de-de", "Lob"),
                     new LocalizedResource("category:suggestion", "en-gb", "suggestion"),
                     new LocalizedResource("category:suggestion", "pl-pl", "sugestia"),
+                    new LocalizedResource("category:suggestion", "de-de", "Vorschlag"),
                     new LocalizedResource("category:issue", "en-gb", "issue"),
                     new LocalizedResource("category:issue", "pl-pl", "problem"),
+                    new LocalizedResource("category:issue", "de-de", "Problem"),
                     new LocalizedResource("category:defect", "en-gb", "defect"),
                     new LocalizedResource("category:defect", "pl-pl", "uszkodzenie"),
+                    new LocalizedResource("category:defect", "de-de", "Defekt"),
                     new LocalizedResource("state:new", "en-gb", "new"),
                     new LocalizedResource("state:new", "pl-pl", "nowe"),
+                    new LocalizedResource("state:new", "de-de", "neu"),
                     new LocalizedResource("state:processing", "en-gb", "processing"),
                     new LocalizedResource("state:processing", "pl-pl", "w trakcie"),
+                    new LocalizedResource("state:processing", "de-de", "in Bearbeitung"),
                     new LocalizedResource("state:resolved", "en-gb", "resolved"),
                     new LocalizedResource("state:resolved", "pl-pl", "rozwiązane"),
+                    new LocalizedResource("state:resolved", "de-de", "abgeschlossen"),
                     new LocalizedResource("state:renewed", "en-gb", "renewed"),
                     new LocalizedResource("state:renewed", "pl-pl", "przywrócone"),
+                    new LocalizedResource("state:renewed", "de-de", "wieder geöffnet"),
                     new LocalizedResource("state:canceled", "en-gb", "canceled"),
-                    new LocalizedResource("state:canceled", "pl-pl", "odrzucone")
+                    new LocalizedResource("state:canceled", "pl-pl", "odrzucone"),
+                    new LocalizedResource("state:canceled", "de-de", "abgebrochen")
                 };
 
                 await _database.LocalizedResources().InsertManyAsync(resources);
