9c1e644e3830b0430c0d1d656ee735ba39d0832f
Piotr Gankiewicz <spetz90@gmail.com>
2017-09-08 14:05:56 +0200

    Updated IoC

--- src/Collectively.Services.Notifications/Framework/Bootstrapper.cs
+++ src/Collectively.Services.Notifications/Framework/Bootstrapper.cs
@@ -78,8 +78,8 @@ namespace Collectively.Services.Notifications.Framework
                 builder.RegisterType<EmailMessageService>().As<IEmailMessageService>();
 
                 var assembly = typeof(Startup).GetTypeInfo().Assembly;
-                builder.RegisterAssemblyTypes(assembly).AsClosedTypesOf(typeof(IEventHandler<>));
-                builder.RegisterAssemblyTypes(assembly).AsClosedTypesOf(typeof(ICommandHandler<>));
+                builder.RegisterAssemblyTypes(assembly).AsClosedTypesOf(typeof(IEventHandler<>)).InstancePerLifetimeScope();
+                builder.RegisterAssemblyTypes(assembly).AsClosedTypesOf(typeof(ICommandHandler<>)).InstancePerLifetimeScope();
                 SecurityContainer.Register(builder, _configuration);
                 RabbitMqContainer.Register(builder, _configuration.GetSettings<RawRabbitConfiguration>());
             });
@@ -88,6 +88,7 @@ namespace Collectively.Services.Notifications.Framework
 
         protected override void RequestStartup(ILifetimeScope container, IPipelines pipelines, NancyContext context)
         {
+            pipelines.SetupTokenAuthentication(container.Resolve<IJwtTokenHandler>());
             pipelines.OnError.AddItemToEndOfPipeline((ctx, ex) =>
             {
                 _exceptionHandler.Handle(ex, ctx.ToExceptionData(),
@@ -116,7 +117,6 @@ namespace Collectively.Services.Notifications.Framework
                 ctx.Response.Headers.Add("Access-Control-Allow-Headers",
                     "Authorization, Origin, X-Requested-With, Content-Type, Accept");
             };
-            pipelines.SetupTokenAuthentication(container);
             _exceptionHandler = container.Resolve<IExceptionHandler>();
             Logger.Information("Collectively.Services.Notifications API has started.");
         }
